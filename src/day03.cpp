a5b262ed5ee40f107d2db2b6e37b5ee6cd3c0903
molen <nothanks@spam.com>
2025-12-13 17:58:04 +0000

    day3

--- src/day03.cpp
+++ src/day03.cpp
@@ -4,35 +4,73 @@
 
 int day3(const stringlist& input)
 {
+    int total = 0;
+
     for (auto& line : input)
     {
-        (void)line;
+        int maxJoltage = 0;
+
+        const auto lineEnd = end(line);
+        for (auto itA = begin(line); (itA+1) != lineEnd; ++itA)
+        {
+            const int jA = (*itA - '0') * 10;
+            for (auto itB = itA + 1; itB != lineEnd; ++itB)
+            {
+                const int jB = (*itB - '0');
+
+                if (jA + jB > maxJoltage)
+                {
+                    maxJoltage = jA + jB;
+                }
+            }
+        }
+
+        total += maxJoltage;
     }
 
-    return -1;
+    return total;
 }
 
-int day3_2(const stringlist& input)
+i64 day3_2(const stringlist& input)
 {
+    constexpr int NumBatteries = 12;
+
+    i64 total = 0;
     for (auto& line : input)
     {
-        (void)line;
+        vector<i8> bats;
+        bats.reserve(line.size());
+        ranges::transform(line, back_inserter(bats), [](char c) { return c - '0'; });
+        
+        i64 running = 0;
+        auto itBatBegin = begin(bats);
+        auto itBatEnd = end(bats) - (NumBatteries - 1);
+        for (int bat = 0; bat < NumBatteries; ++bat)
+        {
+            auto itBiggest = max_element(itBatBegin, itBatEnd);
+            running = 10 * running + *itBiggest;
+            itBatBegin = itBiggest + 1;
+            ++itBatEnd;
+        }
+
+        total += running;
     }
 
-    return -1;
+    return total;
 }
 
 
 void run_day3()
 {
     string sample =
-R"(beep
-boop
-blarp)";
+R"(987654321111111
+811111111111119
+234234234234278
+818181911112111)";
 
-    test(-100, day3(READ(sample)));
-    //gogogo(day3(LOAD(3)));
+    test(357, day3(READ(sample)));
+    gogogo(day3(LOAD(3)));
 
-    //test(-100, day3_2(READ(sample)));
-    //gogogo(day3_2(LOAD(3)));
+    test(3121910778619ll, day3_2(READ(sample)));
+    gogogo(day3_2(LOAD(3)));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ void guarded_main()
 
         run_day1();
         run_day2();
-        //run_day3();
+        run_day3();
         //run_day4();
         //run_day5();
         //run_day6();
