c3f7fb2d6be03ab472511e373d55980987cefd54
molen <nothanks@spam.com>
2025-12-16 17:21:29 +0000

    day6

--- src/day06.cpp
+++ src/day06.cpp
@@ -2,37 +2,90 @@
 #include "harness.h"
 
 
-int day6(const stringlist& input)
+i64 day6(const stringlist& input)
 {
-    for (auto& line : input)
+    vector<vector<i64>> values;
+    for (auto& line : input | views::take(size(input) - 1))
     {
-        (void)line;
+        values.emplace_back(string_to_intlist<i64>(line));
     }
 
-    return -1;
+    i64 total = 0;
+    u64 valueIx = 0;
+    for (const char c : input.back())
+    {
+        if (c == ' ')
+            continue;
+
+        if (c == '*')
+        {
+            i64 prod = 1;
+            for (const auto& row : values)
+                prod *= row[valueIx];
+
+            total += prod;
+        }
+        else if (c == '+')
+        {
+            i64 sum = 0;
+            for (const auto& row : values)
+                sum += row[valueIx];
+
+            total += sum;
+        }
+
+        ++valueIx;
+    }
+
+    return total;
 }
 
-int day6_2(const stringlist& input)
+i64 day6_2(const stringlist& input)
 {
-    for (auto& line : input)
-    {
-        (void)line;
-    }
+    auto inputNums = input | views::take(size(input)-1);
+    const string& inputOps = input.back();
 
-    return -1;
+    i64 total = 0;
+    
+    vector<i64> nums;
+    string num;
+    for (i64 col = isize(input.front()) - 1; col >= 0; --col)
+    {
+        num.clear();
+
+        for (const auto& row : inputNums)
+            num += row[col];
+
+        nums.push_back(stol(num));
+
+        const char op = inputOps[col];
+        if (op == ' ')
+            continue;
+
+        if (op == '*')
+            total += reduce(begin(nums), end(nums), i64(1), multiplies<i64>());
+        else if (op == '+')
+            total += reduce(begin(nums), end(nums), i64(0), plus<i64>());
+
+        nums.clear();
+        --col;  // skip the blank column
+    }
+    
+    return total;
 }
 
 
 void run_day6()
 {
     string sample =
-R"(beep
-boop
-blarp)";
+R"(123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  )";
 
-    test(-100, day6(READ(sample)));
-    //gogogo(day6(LOAD(6)));
+    test(4277556l, day6(READ(sample)));
+    gogogo(day6(LOAD(6)));
 
-    //test(-100, day6_2(READ(sample)));
-    //gogogo(day6_2(LOAD(6)));
+    test(3263827, day6_2(READ(sample)));
+    gogogo(day6_2(LOAD(6)));
 }
